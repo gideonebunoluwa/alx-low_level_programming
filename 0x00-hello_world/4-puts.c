a71b8343d53f4e01b0a0a30660e256539ce4af6b
gideonebunoluwa <ebunoluwagideon@gmail.com>
2022-03-13 10:08:57 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
 #!/bin/bash
+#include <studio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ * /
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
